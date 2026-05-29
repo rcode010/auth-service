b370a1603c33e5404d0d1d3cc6ddc6010c66c573
Raman <ramanazad881@gmail.com>
2026-05-29 22:53:52 +0300

    feat(user): Create User login endpoint

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController
 {
@@ -18,5 +21,24 @@ class AuthController
             "message" => "User created successfully",
             "data"=>$user
         ],201);
+    }
+
+    # Login
+    public function login(LoginRequest $request){
+            $request->validated();
+
+        $token = JWTAuth::attempt($request->only('email','password'));
+        if($token){
+            return response([
+                "success" => true,
+                "message" => "User logged in successfully",
+                "user"=>auth()->user(),
+                "token"=>$token
+            ],200);
+        }
+        return response([
+            "success" => false,
+            "message" => "Invalid email or password"
+        ],401);
     }
 }
