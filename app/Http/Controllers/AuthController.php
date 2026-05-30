6bac20107714986208f14960f9525ced5f967ac7
Raman <ramanazad881@gmail.com>
2026-05-30 23:32:48 +0300

    feat(user): Create an endpoint to retrieve user's profile data

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\ProfileRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -40,5 +41,13 @@ class AuthController
             "success" => false,
             "message" => "Invalid email or password"
         ],401);
+    }
+
+    # profile
+    public function profile(Request $request){
+        return response([
+            "success" => true,
+            "data" => auth()->user()
+        ],200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,4 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/user', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::GET('/profile', [AuthController::class, 'profile'])->middleware('auth:api');
