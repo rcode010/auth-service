5f7989dbeed199ca44c530ffc94529148a13962c
Raman <ramanazad881@gmail.com>
2026-06-02 14:54:44 +0300

    fix(routes): group auth routes under /auth prefix and fix route naming

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix("/auth")->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:login');
+    Route::get('/me', [AuthController::class, 'profile'])->middleware('auth:api');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+    Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
+});
 
-Route::post('/user', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:login');
-Route::GET('/profile', [AuthController::class, 'profile'])->middleware('auth:api');
-Route::POST('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
-Route::POST('/refresh-token', [AuthController::class, 'refreshToken']);
+
